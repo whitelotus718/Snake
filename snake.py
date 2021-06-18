9f3a0b04506e01a1b6c0031245ebb7f559df994b
whitelotus718 <70615488+whitelotus718@users.noreply.github.com>
2021-06-18 16:54:55 -0400

    graph

--- snake.py
+++ snake.py
@@ -27,10 +27,22 @@ class snake(object):
         pass
 
 def drawGrid(w, rows, surface):
-    pass
+    sizeBtwn = w // rows
 
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
+        
 def redrawWindow(surface):
-    pass
+    global rows, width
+    surface.fill((0,0,0))
+    drawGrid(width, rows, surface)
+    pygame.display.udpate()
 
 def randomSnack(rows, items):
     pass
@@ -39,10 +51,10 @@ def message_box(subject, content):
     pass
 
 def main():
+    global width, rows
     width = 500
-    height = 500
     rows = 20
-    win = pygame.display.set_mode((width, height))
+    win = pygame.display.set_mode((width, width))
     s = snake((255,0,0), (10,10))
     flag = True
 
